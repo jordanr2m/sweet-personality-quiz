717903d79df84a911cb8bcf8c41798c46d65fa31
Jordan J <jordanr2m@gmail.com>
2024-05-05 12:11:27 -0700

    Updated wording on chocolate result.

--- src/helpers/Results.js
+++ src/helpers/Results.js
@@ -13,7 +13,7 @@ export const Results = [
     },
     {
         name: "Chocolate",
-        description: "Who doesn't love a classic bar of chocolate, or any type of chocolate, for that matter?! You are like that piece of chocolate. You are easy going and pure by nature, which makes others gravitate towards you. You enjoy spending time with both close family and friends, as well as acquaintances or people you've just met. You are likely a respected colleague as well as a trustworthy friend, and you feel comfortable speaking your mind most of the time. You have much to offer the universe, so keeping your curiosity alive is key. Try to say yes to as many new experiences in life as possible. ◡̈",
+        description: "Who doesn't love a classic bar of chocolate, or any type of chocolate, for that matter?! You are like that piece of chocolate. You are easy going and pure by nature, which makes others gravitate towards you. You enjoy spending time with both close family and friends, as well as acquaintances or people you've just met. You are likely a respected colleague as well as a trustworthy friend, and you feel comfortable speaking your mind most of the time. You have much to offer the universe, and keeping your curiosity alive is key. Try to say yes to as many new experiences in life as possible. ◡̈",
         img: "https://images.unsplash.com/photo-1606310616021-f566a6e9cb07?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         alt: "Assorted pieces of chocolate"
     },
